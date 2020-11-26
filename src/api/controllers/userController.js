a98ca96158b9161de7a8b6e4e13c8befccfe66b9
Snobrost <74957909+Snobrost@users.noreply.github.com>
2020-11-26 09:04:30 +0100

    Verification format email

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -4,22 +4,30 @@ const mdl = require('../../middleware/jwtMiddleware');
 
 
 exports.create_an_user = (req, res) => {
-    let new_user = new User(req.body);
-    new_user.password =mdl.crypt_password(req);
-    new_user.save((error, user) => {
-        if (error) {
-            res.status(500);
-            console.log(error);
-            res.json({
-                message: "Erreur serveur."
-            })
-        } else {
-            res.status(201);
-            res.json({
-                message: `Utilisateur crée : ${user.email}`
-            })
-        }
-    })
+    if(mdl.validateEmail(req.body.email)){
+        let new_user = new User(req.body);
+        new_user.password =mdl.crypt_password(req);
+        new_user.save((error, user) => {
+            if (error) {
+                res.status(500);
+                console.log(error);
+                res.json({
+                    message: "Erreur serveur."
+                })
+            } else {
+                res.status(201);
+                res.json({
+                    message: `Utilisateur crée : ${user.email}`
+                })
+            }
+        })
+    }else {
+        res.status(500);
+        res.json({
+            message: "Email nonconforme."
+        })
+    }
+    
 }
 
 exports.login_an_user = (req, res) => {
--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -53,3 +53,8 @@ exports.crypt_password = (req) => {
     return encryptedPassword;
     
 }
+
+exports.validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
